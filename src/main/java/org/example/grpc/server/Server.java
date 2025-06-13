4e54237e2a27d609d0485494534252d43d623c1a
zhrab <zahraabiri816@gmail.com>
2025-06-13 18:15:54 +0430

    start qrpc service

---
+++ src/main/java/org/example/grpc/server/Server.java
@@ -0,0 +1,90 @@
+package org.example.grpc.server;
+
+import org.example.grpc.StudentRequest;
+import org.example.grpc.StudentResponse;
+import org.example.grpc.StudentServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import org.lognet.springboot.grpc.GRpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GRpcService
+//The Server class extends the generated base class StudentServiceImplBase, which provides default implementations for the RPC methods defined in the protobuf file.
+public class Server extends StudentServiceGrpc.StudentServiceImplBase{
+
+    Logger logger = LoggerFactory.getLogger(Server.class);
+    StudentResponse response = StudentResponse.newBuilder()
+            .setAge(10).setName("Mahmood")
+            .build();
+    @Override
+    public void getStudent(StudentRequest request, StreamObserver<StudentResponse> responseObserver) {
+        logger.info("got request");
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+        logger.info("sent response");
+    }
+
+    @Override
+    public void getStudentStream(StudentRequest request, StreamObserver<StudentResponse> responseObserver) {
+        logger.info("got request");
+        for(int i=0;i<1000000;i++){
+            logger.info("going to send response number "+ i);
+            responseObserver.onNext(response);
+        }
+        responseObserver.onCompleted();
+        logger.info("sent response");
+    }
+
+    @Override
+    public StreamObserver<StudentRequest> sendStudentStream(StreamObserver<StudentResponse> responseObserver) {
+        logger.info("got request");
+        var responseStream = new StreamObserver<StudentRequest>() {
+            @Override
+            public void onNext(StudentRequest studentRequest) {
+                logger.info("received request to server");
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                logger.error("got some error "+ throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                logger.info("completed");
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();
+            }
+        };
+        logger.info("sent response");
+        return responseStream;
+    }
+
+
+    public StreamObserver<StudentRequest> sendAndGetStudentStream(StreamObserver<StudentResponse> responseObserver) {
+        logger.info("going to send and get");
+        var res = new StreamObserver<StudentRequest>() {
+            @Override
+            public void onNext(StudentRequest studentRequest) {
+                logger.info("got the request.. sending response");
+                responseObserver.onNext(response);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                logger.error("got error = "+ throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                logger.info("completed");
+                responseObserver.onCompleted();
+            }
+        };
+
+        logger.info("sent response");
+        return res;
+
+    }
+
+}
