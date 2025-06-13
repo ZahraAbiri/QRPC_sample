64cb011875029a4e1cb006d1b54d57082f26fd2e
zhrab <zahraabiri816@gmail.com>
2025-06-13 18:24:12 +0430

    starter

---
+++ src/main/java/org/example/grpc/GRPCApp.java
@@ -0,0 +1,17 @@
+package org.example.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class GRPCApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(GRPCApp.class, args);
+    }
+}
